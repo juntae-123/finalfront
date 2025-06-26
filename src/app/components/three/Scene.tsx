6c7e0b061b3fa55318be30f6daea2eda9aa4c86e
박준태 <swedf123@gmail.com>
2025-06-26 14:46:20 +0900

    Remove unused event parameter in Scene.tsx

--- src/app/components/three/Scene.tsx
+++ src/app/components/three/Scene.tsx
@@ -47,7 +47,7 @@ const AnimatedText = ({
     }
   });
 
-  const handlePointerMove = (event: ThreeEvent<PointerEvent>) => {
+  const handlePointerMove = () => {
     if (materialRef.current) {
       gsap.to(materialRef.current.color, {
         r: hovered ? 0.4 : 1,
